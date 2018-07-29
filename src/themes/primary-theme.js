3a400d970f921f14c03733f925361279148b61bb
Sipho Hlophe <sipho@iplatform.co.za>
2018-07-29 17:15:01 +0200

    chore: adjust theme

--- src/themes/primary-theme.js
+++ src/themes/primary-theme.js
@@ -1,39 +1,14 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-
-// A theme with custom primary and secondary color.
-// It's optional.
-const secondaryTheme = createMuiTheme({
-  palette: {
-    primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700]
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700]
-    }
-  }
-});
+import blue from '@material-ui/core/colors/blue';
 
 const primaryTheme = createMuiTheme({
   palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff'
-    },
+    primary: blue,
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000'
-    }
+      main: '#40c4ff',
+      contrastText: '#000',
+    },
   }
 });
 
-export { primaryTheme, secondaryTheme };
+export { primaryTheme };
